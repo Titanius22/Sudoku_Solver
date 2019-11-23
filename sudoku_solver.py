59eb80077a108d9f40f97d732f56ed45cdf6b257
Titanius22 <Titanius22@users.noreply.github.com>
2019-11-22 21:38:02 -0800

    works
    completes 0.0314151

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,4 +1,5 @@
 import csv
+import timeit
 
 def return_empty_2d_matrix():
     return [
@@ -55,7 +56,7 @@ def import_input():
 
 def check_row(rowNum):
     #print(input_matrix[rowNum])
-    return set(range(0, 9)).difference(set(input_matrix[rowNum]))
+    return set(range(1, 10)).difference(set(input_matrix[rowNum]))
     
 
 def check_box(rowNum, colNum):
@@ -67,7 +68,7 @@ def check_box(rowNum, colNum):
     for row in range(startingRow, startingRow+3):
         for col in range(startingCol, startingCol+3):
             box.append(input_matrix[row][col])
-    return set(range(0, 9)).difference(set(box))
+    return set(range(1, 10)).difference(set(box))
 
 
 def check_column(colNum):
@@ -75,7 +76,7 @@ def check_column(colNum):
 
     for row in range(0, 9):
         box.append(input_matrix[row][colNum])
-    return set(range(0, 9)).difference(set(box))
+    return set(range(1, 10)).difference(set(box))
 
 
 #Variables
@@ -87,6 +88,8 @@ found_count = 0
 
 def main1():
     
+    start = timeit.default_timer()
+
     global input_matrix
     global box_search_order
     global found_count
@@ -108,12 +111,18 @@ def main1():
     for out_row in input_matrix:
         print(out_row)
     
-    for i in range(0, 10):
+    for i in range(0, 5):
         for box in box_search_order: #for each box
             missing_box_set = check_box(3*box[0], 3*box[1])
+            if len(missing_box_set) < 1:
+                continue
+            print("missing box values")
+            print(missing_box_set)
             for row in range(3*box[0], 3 + (3*box[0])): #each row in box
                 missing_row_set = check_row(row)
-                for col in range(3*box[1], 3 + (3*box[1])): #each col iteam
+                if len(missing_row_set) < 1:
+                    continue
+                for col in range(3*box[1], 3 + (3*box[1])): #each col item
                     if input_matrix[row][col] == 0:
                         missing_col_set = check_column(col)
                         choices_matrix[row][col] = missing_box_set.intersection(missing_row_set, missing_col_set)
@@ -129,6 +138,9 @@ def main1():
             print(out_row)
 
 
+    stop = timeit.default_timer()
+
+    print('Time: ', stop - start) 
 
 if __name__ == '__main__':
     main1()
