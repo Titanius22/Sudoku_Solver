2e8717ad8e33b0721969ad3674d429151d6b1dff
Titanius22 <Titanius22@users.noreply.github.com>
2019-11-22 21:08:08 -0800

    Mostly working
    need to fix
    -two 1s showed up in the same box
    -boxes with only one missing number didn't get solved

---
+++ sudoku_solver.py
@@ -0,0 +1,134 @@
+import csv
+
+def return_empty_2d_matrix():
+    return [
+              [0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0]
+             ]
+
+
+def return_empty_3d_matrix():
+    return [
+              [[],[],[],[],[],[],[],[],[]],
+              [[],[],[],[],[],[],[],[],[]],
+              [[],[],[],[],[],[],[],[],[]],
+              [[],[],[],[],[],[],[],[],[]],
+              [[],[],[],[],[],[],[],[],[]],
+              [[],[],[],[],[],[],[],[],[]],
+              [[],[],[],[],[],[],[],[],[]],
+              [[],[],[],[],[],[],[],[],[]],
+              [[],[],[],[],[],[],[],[],[]]
+             ]
+
+
+def import_input():
+    
+    global zero_count
+
+    matrix = return_empty_2d_matrix()
+
+    with open('sudoku_input.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        row_count = 0
+        for row in csv_reader:
+            column_count = 0
+            for element in row:
+                if element: # if the string is "", then it equals false
+                    matrix[row_count][column_count] = int(element)
+                else:
+                    zero_count += 1
+                column_count += 1
+                if column_count == 9:
+                    break
+            row_count +=1
+            if row_count == 9:
+                break
+    return matrix
+
+
+def check_row(rowNum):
+    #print(input_matrix[rowNum])
+    return set(range(0, 9)).difference(set(input_matrix[rowNum]))
+    
+
+def check_box(rowNum, colNum):
+    box = []
+
+    startingRow = rowNum - (rowNum%3)
+    startingCol = colNum - (colNum%3)
+
+    for row in range(startingRow, startingRow+3):
+        for col in range(startingCol, startingCol+3):
+            box.append(input_matrix[row][col])
+    return set(range(0, 9)).difference(set(box))
+
+
+def check_column(colNum):
+    box = []
+
+    for row in range(0, 9):
+        box.append(input_matrix[row][colNum])
+    return set(range(0, 9)).difference(set(box))
+
+
+#Variables
+input_matrix = []
+box_search_order = []
+zero_count = 0
+found_count = 0
+
+
+def main1():
+    
+    global input_matrix
+    global box_search_order
+    global found_count
+    
+    input_matrix = import_input()
+    choices_matrix = return_empty_3d_matrix()
+    box_search_order = [[0,0],
+                        [0,1],
+                        [0,2],
+                        [1,0],
+                        [1,1],
+                        [2,0],
+                        [1,2],
+                        [2,1],
+                        [2,2]
+                       ]
+
+    
+    for out_row in input_matrix:
+        print(out_row)
+    
+    for i in range(0, 10):
+        for box in box_search_order: #for each box
+            missing_box_set = check_box(3*box[0], 3*box[1])
+            for row in range(3*box[0], 3 + (3*box[0])): #each row in box
+                missing_row_set = check_row(row)
+                for col in range(3*box[1], 3 + (3*box[1])): #each col iteam
+                    if input_matrix[row][col] == 0:
+                        missing_col_set = check_column(col)
+                        choices_matrix[row][col] = missing_box_set.intersection(missing_row_set, missing_col_set)
+                        if len(choices_matrix[row][col]) == 1:
+                            #print("found one!!")
+                            found_count += 1
+                            input_matrix[row][col] = next(iter(choices_matrix[row][col]))
+        
+        print("Done!!")
+        print("zeros: " + str(zero_count))
+        print("found: " + str(found_count))
+        for out_row in input_matrix:
+            print(out_row)
+
+
+
+if __name__ == '__main__':
+    main1()
